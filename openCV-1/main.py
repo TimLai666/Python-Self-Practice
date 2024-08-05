7a5272b42dcb7d8bc4d68dd45c6b4818a5eb34fc
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-08-05 13:29:01 +0800

    upload

--- openCV-1/main.py
+++ openCV-1/main.py
@@ -94,10 +94,45 @@ import numpy as np
 # cv2.waitKey(0)
 
 
-img = np.zeros((600, 600, 3), np.uint8)
-cv2.line(img, (0, 0), (img.shape[1], img.shape[0]), (0, 255, 0), 2)
-cv2.rectangle(img, (0,0), (400, 300), (0, 0, 255), cv2.FILLED)
-cv2.circle(img, (300, 400), 30, (255, 0, 0), cv2.FILLED)
-cv2.putText(img, 'hello', (100, 500), cv2.FONT_HERSHEY_SIMPLEX, 1, (150,150,150), 2)
-cv2.imshow('img', img)
-cv2.waitKey(0)
+# img = np.zeros((600, 600, 3), np.uint8)
+# cv2.line(img, (0, 0), (img.shape[1], img.shape[0]), (0, 255, 0), 2)
+# cv2.rectangle(img, (0,0), (400, 300), (0, 0, 255), cv2.FILLED)
+# cv2.circle(img, (300, 400), 30, (255, 0, 0), cv2.FILLED)
+# cv2.putText(img, 'hello', (100, 500), cv2.FONT_HERSHEY_SIMPLEX, 1, (150,150,150), 2)
+# cv2.imshow('img', img)
+# cv2.waitKey(0)
+
+def empty():
+    return 0
+
+img = cv2.imread('XiWinnie.jpg')
+img = cv2.resize(img,(0,0), fx=0.5, fy=0.5)
+
+cv2.namedWindow('TrackBar')
+cv2.resizeWindow('TrackBar', 640, 320)
+cv2.createTrackbar('Hue Min', 'TrackBar', 0, 179, empty)
+cv2.createTrackbar('Hue Max', 'TrackBar', 179, 179, empty)
+cv2.createTrackbar('Sat Min', 'TrackBar', 0, 255, empty)
+cv2.createTrackbar('Sat Max', 'TrackBar', 255, 255, empty)
+cv2.createTrackbar('Val Min', 'TrackBar', 0, 255, empty)
+cv2.createTrackbar('Val Max', 'TrackBar', 255, 255, empty)
+
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+while True:
+    h_min = cv2.getTrackbarPos('Hue Min', 'TrackBar')
+    h_max = cv2.getTrackbarPos('Hue Max', 'TrackBar')
+    s_min = cv2.getTrackbarPos('Sat Min', 'TrackBar')
+    s_max = cv2.getTrackbarPos('Sat Max', 'TrackBar')
+    v_min = cv2.getTrackbarPos('Val Min', 'TrackBar')
+    v_max = cv2.getTrackbarPos('Val Max', 'TrackBar')
+
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(hsv, lower, upper)
+    result = cv2.bitwise_and(img, img, mask=mask)
+
+    cv2.imshow('img', img)
+    cv2.imshow('hsv',hsv)
+    cv2.imshow('mask',mask)
+    cv2.imshow('result',result)
+    cv2.waitKey(1)
